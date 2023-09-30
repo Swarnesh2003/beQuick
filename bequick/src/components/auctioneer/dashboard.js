26015fe8ba97f39f1f38fd68c961283e7dec0d9d
Swarneshwar Senthilkumar <swarnesh9421@gmail.com>
2023-09-30 11:59:14 +0530

    alignment changes in dashboard

--- bequick/src/components/auctioneer/dashboard.js
+++ bequick/src/components/auctioneer/dashboard.js
@@ -4,7 +4,14 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
+import Avatar from '@mui/material/Avatar';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import GavelIcon from '@mui/icons-material/Gavel';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import HourglassEmptyRoundedIcon from '@mui/icons-material/HourglassEmptyRounded';
+import DownloadDoneRoundedIcon from '@mui/icons-material/DownloadDoneRounded';
+import AccountBalanceRoundedIcon from '@mui/icons-material/AccountBalanceRounded';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -73,32 +80,77 @@ export default function Dashboard() {
 
   return (
     <Box>
-        <Box sx={{height:'9vh',marginBottom:0.5,boxShadow: '0px 4px 6px rgb(236, 249, 255)'}}>
-            <Box>
+        <Box sx={{height:'9vh',marginBottom:0.5,boxShadow: '0px 4px 6px rgb(236, 249, 255)', display:'flex', alignItems:'center'}}>
+            <Box sx={{display:'flex', alignItems:'center', marginRight:2}}>
+            <Box sx={{display:'flex', alignItems:'center'}}>
             <img 
             src="..\images\logo.png"
             alt="Auction"
-            style={{width:'12vw',height:'auto', marginLeft:40,marginRight:10,marginBottom:2}} />
+            style={{width:'12vw',height:'auto', marginLeft:30,marginRight:10}} />
             </Box>
-            <Box>
+            <Box sx={{marginLeft:6,display:'flex', alignItems:'center'}}>
+              <Typography >Dashboard</Typography>
+            </Box>
+            </Box>
 
+            <Box sx={{position:'absolute', right:0, marginRight:2, display:'flex', flexDirection:'row', alignItems:'center'}}>
+              <Box sx={{display:'flex', alignItems:'center' , marginRight:4}}> <img 
+            src="..\images\notification.png"
+            alt="Auction"
+            style={{width:'1.5vw',height:'auto'}} /></Box>
+           
+            <Box sx={{display:'flex', alignItems:'center', marginRight:2}}>
+            <Avatar sx={{width:'2.5vw', height:'auto'}} alt="Remy Sharp" src="..\images\images.jfif" />
+            <Typography sx={{marginLeft: 1,marginRight:0.7, fontSize:15}}>Swarnesh</Typography>
+            <ArrowDropDownIcon color="primary" />
             </Box>
+           </Box>
         </Box>
     <Box
       sx={{ flexGrow: 3, bgcolor: 'rgb(236, 249, 255)', display: 'flex', height: '110vh' }}
     >
       <Tabs 
         orientation="vertical"
-        variant="scrollable"
+        
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider',width:'17vw',bgcolor: 'white' }}
+        sx={{ borderRight: 1, borderColor: 'divider',width:'16vw',bgcolor: 'white' }}
       >
-        <LinkTab label="New Auction" href='#'/>
-        <LinkTab label="Ongoing Auction" href="#" />
-        <LinkTab label="Closed Auction" href="#" />
-        <LinkTab label="Transaction" href="#" />
+      <LinkTab sx={{marginTop:'3vh'}} label={
+        <div style={{display:'flex', alignItems: 'center', position:'absolute', left:'1.5vw'}}>
+          
+            <GavelIcon /> {/* Replace with your desired icon */}
+          
+          <Typography sx={{marginLeft:2, fontSize:12}}>New Auction</Typography>
+        </div>
+      } href='#'/>
+      <LinkTab label={
+        <div style={{display:'flex', alignItems: 'center',  position:'absolute',left:'1.5vw'}}>
+          
+            <HourglassEmptyRoundedIcon /> {/* Replace with your desired icon */}
+          
+            <Typography sx={{marginLeft:2, fontSize:12}}>Ongoing Auction</Typography>
+        </div>
+      } href='#'/>
+      <LinkTab label={
+        <div style={{display:'flex', alignItems: 'center', position:'absolute',left:'1.5vw'}}>
+          
+            < DownloadDoneRoundedIcon /> {/* Replace with your desired icon */}
+          
+            <Typography sx={{marginLeft:2, fontSize:12}}>Closed Auctions</Typography>
+        </div>
+      } href='#'/>
+        
+        
+        <LinkTab label={
+        <div style={{display:'flex',alignItems: 'center', position:'absolute', left:'1.5vw'}}>
+          
+            < AccountBalanceRoundedIcon /> {/* Replace with your desired icon */}
+          
+            <Typography sx={{marginLeft:2, fontSize:12}}>Transactions</Typography>
+        </div>
+      } href='#'/>
       </Tabs>
 
     </Box>
