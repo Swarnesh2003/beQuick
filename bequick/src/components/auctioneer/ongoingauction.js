d4147a198ba353baeb838552b3ebbaca1ad26c73
Thirusha <thirusha1030@gmail.com>
2023-10-01 07:57:16 +0530

    new auction inputs has been updated

--- bequick/src/components/auctioneer/ongoingauction.js
+++ bequick/src/components/auctioneer/ongoingauction.js
@@ -1,11 +1,27 @@
+import { Grid } from '@mui/material';
 import React from 'react';
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
 
-
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 export default function Ongoingauction(){
     return(
-        <div className='home'>
-           <a href="/login"><button>Login</button></a> 
-            <a href="/signup"><button>Signup</button></a>
-        </div>
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={8} lg={6} sx={{height:"30vh"}}>
+                <Item ></Item>
+            </Grid>
+            <Grid item xs={12} md={8} lg={6}>
+                <Item ></Item>
+            </Grid>
+            
+
+        </Grid>
     )
 }
